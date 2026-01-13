3fc78bcf20b9772a30d058ac79fec81143e2b5c6
Saaria-Zahid <saariazahid07@gmail.com>
2026-01-13 20:06:15 +0500

    Modules Import

--- 02_moduleExport.js
+++ 02_moduleExport.js
@@ -1,5 +1,4 @@
 
-
 const users = [
     {
         name: "Saaria",
---
+++ 03_moduleImport.js
@@ -0,0 +1,13 @@
+
+const users = require('./02_moduleExport');
+
+console.log("Users Info:", users);
+
+
+// Output:
+// Users Info: [
+//   { name: 'Saaria', age: 17, developer: true },
+//   { name: 'Asim', age: 20, developer: false },
+//   { name: 'Yousuf', age: 2, developer: true }
+// ]
+
