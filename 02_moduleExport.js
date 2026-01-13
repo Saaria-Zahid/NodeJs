c5b8d01ef60c7f5a98870c8212f36cad4086ec04
Saaria-Zahid <saariazahid07@gmail.com>
2026-01-13 19:50:25 +0500

    Module Export

---
+++ 02_moduleExport.js
@@ -0,0 +1,20 @@
+
+
+const users = [
+    {
+        name: "Saaria",
+        age: 17,
+        developer: true
+    },
+    {
+        name: "Asim",
+        age: 20,
+        developer: false
+    },
+    {
+        name: "Yousuf",
+        age: 2,
+        developer: true
+    }
+]
+module.exports = users;
