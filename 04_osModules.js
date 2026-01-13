34df899834e7be56974ed1a15a1cbfaa0783cb8b
Saaria-Zahid <saariazahid07@gmail.com>
2026-01-13 20:39:09 +0500

    OS Modules

---
+++ 04_osModules.js
@@ -0,0 +1,10 @@
+const os = require('os');
+
+console.log("Operating System Info:");
+console.log("Type:", os.type());
+console.log("Platform:", os.platform());
+console.log("Architecture:", os.arch());
+console.log("CPU Info:", os.cpus());
+console.log("Total Memory:", os.totalmem());
+console.log("Free Memory:", os.freemem());
+console.log("Hostname:", os.hostname());
